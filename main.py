4486344a202c04ec9adee82d20e6b32cec622253
Aarya <111475919+rya23@users.noreply.github.com>
2024-08-29 23:42:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ async def read_item(item_name):
     # with open("ministers1.json", "r", encoding="utf-8") as file:
     #     ministers_data = json.load(file)
     output_dict = [x for x in ministers_data if x["name"].lower() == item_name.lower()]
-    print(type(output_dict))
     # if not output_dict:
     # raise HTTPException(status_code=404, detail="Item not found")
     return JSONResponse(content=output_dict)
