fce3bc50c1e3343a3afcc3fc45b5dc6cc549b722
Aarya <111475919+rya23@users.noreply.github.com>
2024-08-29 23:42:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+
 import json
 
 app = FastAPI()
@@ -16,8 +18,8 @@ async def root():
 async def read_item(item_name):
     # with open("ministers1.json", "r", encoding="utf-8") as file:
     #     ministers_data = json.load(file)
-    # output_dict = [x for x in ministers_data if x["name"].lower() == item_name.lower()]
-
-    if not ministers_data:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return ministers_data
+    output_dict = [x for x in ministers_data if x["name"].lower() == item_name.lower()]
+    print(type(output_dict))
+    # if not output_dict:
+    # raise HTTPException(status_code=404, detail="Item not found")
+    return JSONResponse(content=output_dict)
